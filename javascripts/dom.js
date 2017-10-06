124640d834549b7a4f66c20579b8bf5a3de409b4
Jess Brawner <jessnbillbrawner@gmail.com>
2017-10-06 11:47:46 -0500

    built dom string for catCard

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,1 +1,36 @@
 "use strict";
+
+const domString = (kitties) => {
+	let catString = '';
+	for(let i = 0; i < kitties.length; i++) {
+		let catCard = '';
+		catCard += `<div class="cat-card">
+			  			<div class="image-container">
+			    			<img src="IMAGELINK">
+			  			</div>
+			  			<div class="description-container">
+						    <h3> CAT NAME </h3>
+						    <p> Color: COLORRRR </p>
+						    <p> Skills: SKILLLLLZ </p>`;
+		
+		if(kitties[i].toeCount <= 10) {
+			catCard +=		`<p class="disabled-cat"> Toes: TOESSSS </p>`;
+		
+		}else {
+			catCard +=		`<p> Toe Count: ${kitties[i].toeCount} </p>`;
+		}
+
+		catCard +=	  		`</div>
+					</div>`;
+
+		catString += catCard;
+	}
+
+	printToDom(catString);
+};
+
+const printToDom = (strang) => {
+	$('#catContainer').html(strang);
+};
+
+module.exports = {domString};
